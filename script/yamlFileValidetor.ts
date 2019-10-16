e42f075cc7c7d1aeb6564ecb4d8736618aa09f69
SaraGamzu <Sari.Gamzu@gmail.com>
2019-10-16 14:06:34 +0300

    Update yamlFileValidetor.ts

--- script/yamlFileValidetor.ts
+++ script/yamlFileValidetor.ts
@@ -1,1 +1,32 @@
+import { devOps, cli } from '@azure/avocado';
+const YamlValidator = require('yaml-validator');
 
+const main = async () => {
+  const config = cli.defaultConfig();
+  const pr = await devOps.createPullRequestProperties(config);
+  if (pr === undefined) {
+    logWarn("Not in CI environment. Run against all the spec json.");
+    return verboseExec(`???????????????????`);
+  }
+
+  const changedJsonFiles = await pr.structuralDiff()
+    .filter(filePath => filePath.endsWith('.yml') && filePath.startsWith('TestFolder/'))
+    .toArray();
+  if (changedJsonFiles.length === 0) {
+    logWarn("No changed spec json file");
+    return 0;
+  }
+
+  let retCode = 0;
+  const validator = new YamlValidator();
+    validator.validate(changedJsonFiles);
+    validator.report();
+}
+
+
+main().then(retCode => {
+  if (retCode !== 0) {
+    logError('Please fix the error or add words to ./custom-words.txt');
+  }
+  process.exit(retCode);
+});
