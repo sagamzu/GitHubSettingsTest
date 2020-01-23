039df922c7fbc152d75dccc15152fb4c0efd2984
SaraGamzu <Sari.Gamzu@gmail.com>
2020-01-23 12:58:56 +0200

    Update yamlFileValidetor.ts

--- script/yamlFileValidetor.ts
+++ script/yamlFileValidetor.ts
@@ -4,21 +4,15 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 
 export async function IsValidYamlFile(filePath:string): Promise<number> {
-    try {
         yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
         return ExitCode.SUCCESS;
-    }
-    catch (e) {
-        console.error(`Incorrect yaml file. Tile path: ${filePath}. Error message: ${e.message}`);
-        return ExitCode.ERROR;
-    }
 }
 
 let fileTypeSuffixes = ["yaml","yml"];
 let CheckOptions = {
   onCheckFile: (filePath: string, context: CheckContext) => {
       console.log(filePath);
-      return context.exec(`cat ${filePath}`);
+      return IsValidYamlFile(filePath);
   },
   onExecError:async(e:any) => {
       console.log(`ERROR: incorrect yaml files  ${e.message}`);
